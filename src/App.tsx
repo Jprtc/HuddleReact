71be971484c1ae063e261df4ac1adcc01c36ab4a
Joao Pedro de Rossi <jprtc8@gmail.com>
2021-10-19 15:16:26 -0300

    Global Styles created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import {ThemeProvider} from 'styled-components'
+import Header from './Components/Header/'
+import {Container} from './Components/Container/Container.styled'
+import GlobalStyle from './GlobalStyle'
+
+const theme ={
+  colors:{
+    header: '#ebfbff',
+    body:'#fff',
+    footer:'#003333',
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ThemeProvider theme={theme}>
+    <>
+      <GlobalStyle/>
+      <Header/>
+      <Container>
+        <h1>Hello World!</h1>
+      </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,13 @@
+import {StyledHeader} from './style'
+import {Container} from '../Container/Container.styled'
+
+
+export default function Header(){
+    return(
+        <StyledHeader>
+            <Container>
+            <h1>Hubble</h1>
+            </Container>
+        </StyledHeader>
+    )
+}
